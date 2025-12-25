2a4406a8c2bc4edc5498bac90dc9f5bbdf9ee8be
azovnorth <matvii.ianchenko@gmail.com>
2025-12-25 13:41:21 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
 self.addEventListener('install', () => {
   self.skipWaiting();
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(err => console.log('SW registration failed:', err));
+}
